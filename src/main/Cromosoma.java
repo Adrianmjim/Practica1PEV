d01cc221ae90a566691e3335e4eccc06158792c4
adrianmjimuni <adrima05@ucm.es>
2017-02-28 16:22:46 +0100

    Cromosoma

---
+++ src/main/Cromosoma.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Cromosoma {
+	private boolean[] genes;
+	private int fenotipo;
+	private double aptitud;
+	private double puntuacion;
+	private double acumulada;
+	public Cromosoma() {
+		
+	}
+	
+}
